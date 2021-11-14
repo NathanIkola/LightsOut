afa2606860aa1d11cf02dfb3df299842744f89f0
Nathan Ikola <nathan.ikola@gmail.com>
2021-11-14 15:25:29 -0600

    Re-support neural supercharger
    The new way of hijacking the power draw caused issues with the neural
    supercharger, so fix that

---
+++ Source/LightsOutSln/LightsOut/Patches/Power/RechargeablePatch.cs
@@ -0,0 +1,31 @@
+﻿//************************************************
+// Patch the neural supercharger
+//************************************************
+
+using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using ModSettings = LightsOut.Boilerplate.ModSettings;
+
+namespace LightsOut.Patches.Power
+{
+    [HarmonyPatch(typeof(CompRechargeable))]
+    [HarmonyPatch(nameof(CompRechargeable.CompTick))]
+    public class RechargeablePatch
+    {
+        public static void Prefix(CompRechargeable __instance, CompPowerTrader ___compPowerCached)
+        {
+            if (___compPowerCached is null) return;
+            
+            float powerDraw = ___compPowerCached.Props.basePowerConsumption;
+            if(powerDraw > 0)
+            {
+                if (__instance.Charged) 
+                    powerDraw *= ModSettings.StandbyPowerDrawRate;
+                else 
+                    powerDraw *= ModSettings.ActivePowerDrawRate;
+                ___compPowerCached.powerOutputInt = powerDraw;
+            }
+        }
+    }
+}
