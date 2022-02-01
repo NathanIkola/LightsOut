3b1cdc9162849aab933666bc8bb10b5f3ff37250
Nathan Ikola <nathan.ikola@gmail.com>
2022-01-31 18:59:35 -0600

    Clean up the repo
    Remove unnecessary usings
    
    Delete unnecessary patch files

--- Source/LightsOutSln/LightsOut/Patches/ModCompatibility/ICompatibilityPatchComponent.cs
+++ Source/LightsOutSln/LightsOut/Patches/ModCompatibility/ICompatibilityPatchComponent.cs
@@ -5,11 +5,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
-using Verse;
 
 namespace LightsOut.Patches.ModCompatibility
 {
