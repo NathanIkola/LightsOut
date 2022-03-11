e2eea98e7011ee56dbfcaf136542e5a5f2a17eed
Nathan Ikola <nathan.ikola@gmail.com>
2022-03-10 22:38:54 -0600

    Roll back last commit
    Change back to old way of checking for pawns. There was no real benefit
    to doing it the new way since it likely has to search through all
    animals too (which would be slower) and it could introduce more
    collection modification bugs. Not worth the risk atm

--- Source/LightsOutSln/LightsOut/Common/Rooms.cs
+++ Source/LightsOutSln/LightsOut/Common/Rooms.cs
@@ -18,15 +18,41 @@ namespace LightsOut.Common
         public static bool RoomIsEmpty(Room room, Pawn pawn)
         {
             if (room is null || room.OutdoorsForWork || room.IsDoorway
-                || !(room.Map?.regionAndRoomUpdater?.Enabled ?? false)
-                || room.Map?.mapPawns.AllPawns is null) return false;
+                || !(room.Map?.regionAndRoomUpdater?.Enabled ?? false)) return false;
 
-            foreach (Pawn otherPawn in room.Map.mapPawns.AllPawns)
-                if (otherPawn.GetRoom() == room && otherPawn.RaceProps.Humanlike && otherPawn != pawn
-                        // what if two pawns were both leaving the room at the same time haha... unless?
-                        && (otherPawn.pather.nextCell.GetEdifice(otherPawn.Map) as Building_Door) == null
-                        // what if a pawn is entering while another pawn is leaving haha... unless??
-                        && (otherPawn.Position.GetEdifice(otherPawn.Map) as Building_Door) == null)
+            bool done = false;
+            uint attempts = 0;
+            Thing[] things = null;
+            while (!done)
+            {
+                try
+                {
+                    things = room.ContainedAndAdjacentThings.ToArray();
+                    done = true;
+                }
+                catch (InvalidOperationException e)
+                {
+                    if (e.Message.ToLower().Contains("modified"))
+                    {
+                        if (++attempts > 100) done = true;
+                    }
+                    else
+                    {
+                        Log.Warning($"[LightsOut](RoomIsEmpty): InvalidOperationException: {e.Message}");
+                        done = true;
+                    }
+                }
+            }
+            if (attempts > 1)
+                Log.Warning($"[LightsOut](RoomIsEmpty): collection was unexpectedly updated {attempts} time(s). If this number is big please report it.");
+
+            // now actually go through the collection
+            foreach (Thing thing in things)
+                if (thing is Pawn otherPawn && otherPawn.RaceProps.Humanlike && otherPawn != pawn
+                    // what if two pawns were both leaving the room at the same time haha... unless?
+                    && (otherPawn.pather.nextCell.GetEdifice(otherPawn.Map) as Building_Door) == null
+                    // what if a pawn is entering while another pawn is leaving haha... unless??
+                    && (otherPawn.Position.GetEdifice(otherPawn.Map) as Building_Door) == null)
                 {
                     return false;
                 }
@@ -42,15 +68,41 @@ namespace LightsOut.Common
         public static bool AllPawnsSleeping(Room room, Pawn pawn)
         {
             if (room is null || room.OutdoorsForWork || room.IsDoorway
-                || !(room.Map?.regionAndRoomUpdater?.Enabled ?? false)
-                || room.Map?.mapPawns.AllPawns is null) return false;
+                || !(room.Map?.regionAndRoomUpdater?.Enabled ?? false)) return false;
 
-            foreach (Pawn otherPawn in room.Map.mapPawns.AllPawns)
-                if (otherPawn.GetRoom() == room && otherPawn.RaceProps.Humanlike && otherPawn != pawn
-                        // what if two pawns were both leaving the room at the same time haha... unless?
-                        && (otherPawn.pather.nextCell.GetEdifice(otherPawn.Map) as Building_Door) == null
-                        // what if a pawn is entering while another pawn is leaving haha... unless??
-                        && (otherPawn.Position.GetEdifice(otherPawn.Map) as Building_Door) == null)
+            bool done = false;
+            uint attempts = 0;
+            Thing[] things = null;
+            while (!done)
+            {
+                try
+                {
+                    things = room.ContainedAndAdjacentThings.ToArray();
+                    done = true;
+                }
+                catch (InvalidOperationException e)
+                {
+                    if (e.Message.ToLower().Contains("modified"))
+                    {
+                        if (++attempts > 100) done = true;
+                    }
+                    else
+                    {
+                        Log.Warning($"[LightsOut](AllPawnsSleeping): InvalidOperationException: {e.Message}");
+                        done = true;
+                    }
+                }
+            }
+            if (attempts > 1)
+                Log.Warning($"[LightsOut](AllPawnsSleeping): collection was unexpectedly modified {attempts} time(s). If this number is big please report it.");
+
+            // now actually go through the collection
+            foreach (Thing thing in things)
+                if (thing is Pawn otherPawn && otherPawn.RaceProps.Humanlike && otherPawn != pawn
+                    // what if two pawns were both leaving the room at the same time haha... unless?
+                    && (otherPawn.pather.nextCell.GetEdifice(otherPawn.Map) as Building_Door) == null
+                    // what if a pawn is entering while another pawn is leaving haha... unless??
+                    && (otherPawn.Position.GetEdifice(otherPawn.Map) as Building_Door) == null)
                 {
                     if (otherPawn.jobs?.curDriver?.asleep != true)
                         return false;
