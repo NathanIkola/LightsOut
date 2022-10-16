a2f738abc5e3f83d3d03a9b030166c6de8099117
Nathan Ikola <nathan.ikola@gmail.com>
2022-10-15 19:33:19 -0500

    Add documentation for DBH
    Add documentation for the DBH compatibility patch

--- Source/LightsOutSln/LightsOut/Patches/ModCompatibility/DubsBadHygiene/DubsBadHygieneCompatibilityPatch.cs
+++ Source/LightsOutSln/LightsOut/Patches/ModCompatibility/DubsBadHygiene/DubsBadHygieneCompatibilityPatch.cs
@@ -5,6 +5,9 @@
         public override string CompatibilityPatchName => "DubsBadHygiene";
         public override string TargetMod => "Dubs Bad Hygiene";
 
+        /// <summary>
+        /// Add "CeilingFan" to the list of illegal phrases for def names
+        /// </summary>
         public override void OnAfterPatchApplied()
         {
             Common.Lights.AddIllegalLightName("CeilingFan");
