4a96dc401ad25a68ab032e5886ab932f11657b10
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-12-20 17:57:16 +0000

    feat: inspect message showing correctly for charger

--- Source/LightsOutSln/LightsOut/Patches/ModCompatibility/Biotech/PatchMechRecharger.cs
+++ Source/LightsOutSln/LightsOut/Patches/ModCompatibility/Biotech/PatchMechRecharger.cs
@@ -5,6 +5,7 @@ using HarmonyLib;
 using LightsOut.Common;
 using LightsOut.Patches.Power;
 using RimWorld;
+using Verse;
 
 namespace LightsOut.Patches.ModCompatibility.Biotech
 {
@@ -14,13 +15,33 @@ namespace LightsOut.Patches.ModCompatibility.Biotech
     public class PatchMechRecharger : ICompatibilityPatchComponent<PatchMechRecharger>
     {
         public override string ComponentName => "Patch for mech recharger power draw";
+
         public override IEnumerable<PatchInfo> GetPatches(Type type)
         {
-            Glowers.CompGlowers.Add(type);
-            return new List<PatchInfo>{new PatchInfo{method = GetMethod<DisableBasePowerDrawOnGet>(nameof(DisableBasePowerDrawOnGet.Postfix)), patch = GetMethod<PatchMechRecharger>(nameof(Pre_DontAdjustPower)), patchType = PatchType.Prefix}};
+            return new List<PatchInfo>
+            {
+                new PatchInfo
+                {
+                    method = GetMethod<DisableBasePowerDrawOnGet>(nameof(DisableBasePowerDrawOnGet.Postfix)),
+                    patch = GetMethod<PatchMechRecharger>(nameof(Pre_DontAdjustPower)),
+                    patchType = PatchType.Prefix
+                },
+                new PatchInfo
+                {
+                    method = GetMethod(typeof(Tables), nameof(Tables.IsTable)),
+                    patch = GetMethod<PatchMechRecharger>(nameof(Post_IsTable)),
+                    patchType = PatchType.Postfix
+                },
+                new PatchInfo
+                {
+                    method = GetMethod<AddStandbyInspectMessagePatch>(nameof(AddStandbyInspectMessagePatch.Postfix)),
+                    patch = GetMethod<PatchMechRecharger>(nameof(Pre_AddStandbyInspect)),
+                    patchType = PatchType.Prefix
+                },
+            };
         }
 
-        private static FieldInfo IsAttachedToMech = null;
+        private static PropertyInfo IsAttachedToMech = null;
 
         /// <summary>
         ///
@@ -29,22 +50,40 @@ namespace LightsOut.Patches.ModCompatibility.Biotech
         /// <returns></returns>
         private static bool Pre_DontAdjustPower(CompPowerTrader __0)
         {
-            if (__0?.parent is null)
+            if (__0?.parent == null || !IsCharger(__0.parent))
             {
                 return true;
             }
+            return ChargerInUse(__0.parent);
+        }
 
-            if (__0.parent.GetType().Name != "Building_MechRecharger")
-            {
-                return true;
-            }
+        private static bool IsCharger(ThingWithComps thing)
+        {
+            return thing?.GetType().Name == "Building_MechCharger";
+        }
 
+        private static bool ChargerInUse(ThingWithComps thing)
+        {
             if (IsAttachedToMech == null)
             {
-                IsAttachedToMech = AccessTools.Field(__0.parent.GetType(), "IsAttachedToMech");
+                IsAttachedToMech = thing.GetType().GetProperty("IsAttachedToMech", BindingFlags.NonPublic | BindingFlags.Instance);
             }
 
-            return (bool)IsAttachedToMech.GetValue(__0.parent);
+            return (bool)IsAttachedToMech.GetValue(thing);
+        }
+
+        private static void Post_IsTable(ThingWithComps __0, ref bool __result)
+        {
+            __result = __result || IsCharger(__0);
+        }
+
+        private static bool Pre_AddStandbyInspect(ThingWithComps __0)
+        {
+            if (!IsCharger(__0))
+            {
+                return true;
+            }
+            return ChargerInUse(__0);
         }
     }
 }
