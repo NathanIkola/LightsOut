8fc7d8090fb22d60a3604c9808fdf41ca450b090
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-12-20 17:18:16 +0000

    chore: add groundwork

---
+++ Source/LightsOutSln/LightsOut/Patches/ModCompatibility/Biotech/BiotechCompatibilityPatch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using LightsOut.Patches.ModCompatibility.Ideology;
+
+namespace LightsOut.Patches.ModCompatibility.Biotech
+{
+    public class BiotechCompatibilityPatch : ICompatibilityPatch
+    {
+        
+        public override string CompatibilityPatchName => "Biotech";
+        public override string TargetMod => "Biotech";
+        
+        public override IEnumerable<ICompatibilityPatchComponent> GetComponents()
+        {
+            List<ICompatibilityPatchComponent> components = new List<ICompatibilityPatchComponent>()
+            {
+                new PatchMechRecharger()
+            };
+
+            return components;
+        }
+        
+    }
+}
---
+++ Source/LightsOutSln/LightsOut/Patches/ModCompatibility/Biotech/PatchMechRecharger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using LightsOut.Common;
+using LightsOut.Patches.Power;
+using RimWorld;
+
+namespace LightsOut.Patches.ModCompatibility.Biotech
+{
+    /// <summary>
+    /// Disable power draw on mech recharger if not in use by mech
+    /// </summary>
+    public class PatchMechRecharger : ICompatibilityPatchComponent<PatchMechRecharger>
+    {
+        public override string ComponentName => "Patch for mech recharger power draw";
+        public override IEnumerable<PatchInfo> GetPatches(Type type)
+        {
+            Glowers.CompGlowers.Add(type);
+            return new List<PatchInfo>{new PatchInfo{method = GetMethod<DisableBasePowerDrawOnGet>(nameof(DisableBasePowerDrawOnGet.Postfix)), patch = GetMethod<PatchMechRecharger>(nameof(Pre_DontAdjustPower)), patchType = PatchType.Prefix}};
+        }
+
+        private static FieldInfo IsAttachedToMech = null;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="__0"></param>
+        /// <returns></returns>
+        private static bool Pre_DontAdjustPower(CompPowerTrader __0)
+        {
+            if (__0?.parent is null)
+            {
+                return true;
+            }
+
+            if (__0.parent.GetType().Name != "Building_MechRecharger")
+            {
+                return true;
+            }
+
+            if (IsAttachedToMech == null)
+            {
+                IsAttachedToMech = AccessTools.Field(__0.parent.GetType(), "IsAttachedToMech");
+            }
+
+            return (bool)IsAttachedToMech.GetValue(__0.parent);
+        }
+    }
+}
