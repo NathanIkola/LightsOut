10eb451dfe1c42fb0df5fbe1c16624cf6a0ac58b
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-01-11 21:41:23 +0000

    chore: add docs on enterable

--- Source/LightsOutSln/LightsOut/Common/Enterables.cs
+++ Source/LightsOutSln/LightsOut/Common/Enterables.cs
@@ -5,10 +5,18 @@ using Verse;
 
 namespace LightsOut.Common
 {
+    /// <summary>
+    /// Helper functions for enterable buildings support
+    /// </summary>
     public static class Enterables
     {
         private static HashSet<Type> enterables = new HashSet<Type>();
 
+        /// <summary>
+        /// Is a thing registered as an enterable we care about?
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         public static bool IsEnterable(ThingWithComps t)
         {
             return enterables.Contains(t.GetType());
@@ -19,6 +27,10 @@ namespace LightsOut.Common
             return thing is Building_Enterable ent && ent.SelectedPawn != null;
         }
 
+        /// <summary>
+        /// Register type as an enterable that we care about
+        /// </summary>
+        /// <param name="t"></param>
         public static void RegisterEnterable(Type t)
         {
             DebugLogger.Assert(t.IsSubclassOf(typeof(Building_Enterable)), "IsEnterable called with non-enterable");
