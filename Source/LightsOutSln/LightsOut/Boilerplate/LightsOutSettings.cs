ec57f18429173d6f29b1f217ee8e7c9fc25aa320
NathanIkola <52811935+NathanIkola@users.noreply.github.com>
2025-06-08 00:13:41 -0500

    Restore old settings from HugsLib
    Restore settings from HugsLib so that users don't need to manually
    reconfigure the mod the way they had it before removing HugsLib
    
    Fix some settings parsing to make it more usable

--- Source/LightsOutSln/LightsOut/Boilerplate/LightsOutSettings.cs
+++ Source/LightsOutSln/LightsOut/Boilerplate/LightsOutSettings.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
 using HarmonyLib;
 using LightsOut.Common;
 using Verse;
@@ -12,19 +15,41 @@ namespace LightsOut.Boilerplate
     public class LightsOutSettings : ModSettings
     {
         /// <summary>
+        /// Initializes the settings object
+        /// </summary>
+        public LightsOutSettings()
+        {
+            // the mod used to use HugsLib for managing settings, but doesn't anymore
+            // but I don't want to make people angry by resetting all of their settings
+            // so manually find and parse their old settings as the new defaults so that
+            // they don't even notice that I changed anything
+            TryRestoreSettingsFromHugsLib();
+        }
+
+        /// <summary>
         /// Control whether or not Pawns flick lights on and off
         /// </summary>
         public static bool FlickLights = true;
 
         /// <summary>
+        /// The percentage form of the draw rate
+        /// </summary>
+        private static int StandbyResourceDrawPercent = 0;
+
+        /// <summary>
         /// Latent power draw of things when they're flicked off
         /// </summary>
-        public static float StandbyResourceDrawRate = 0f;
+        public static float StandbyResourceDrawRate => StandbyResourceDrawPercent / 100f;
+
+        /// <summary>
+        /// The percentage form of the draw rate
+        /// </summary>
+        public static int ActiveResourceDrawPercent = 100;
 
         /// <summary>
         /// The resource draw rate of things when they're in-use
         /// </summary>
-        public static float ActiveResourceDrawRate = 1f;
+        public static float ActiveResourceDrawRate => ActiveResourceDrawPercent / 100f ;
 
         /// <summary>
         /// Control whether or not Pawns shut off the lights when
@@ -41,7 +66,7 @@ namespace LightsOut.Boilerplate
         /// A list of message filters to add.
         /// Allows messages based on their debug message key.
         /// </summary>
-        public static string[] MessageFilters = { };
+        public static string[] MessageFilters = { DebugMessageKeys.Error + DebugMessageKeys.Mods };
 
         /// <summary>
         /// The number of ticks to delay turning a light off by
@@ -66,12 +91,10 @@ namespace LightsOut.Boilerplate
         public void DrawSettingsMenu(Listing_Standard settingListing)
         {
             bool flickLights = FlickLights;
-            int standbyRate = (int)(StandbyResourceDrawRate * 100);
-            int activeRate = (int)(ActiveResourceDrawRate * 100);
 
             // initialize the input buffers if they haven't been already
-            _standbyBuf = _standbyBuf ?? standbyRate.ToString();
-            _activeBuf = _activeBuf ?? activeRate.ToString();
+            _standbyBuf = _standbyBuf ?? StandbyResourceDrawPercent.ToString();
+            _activeBuf = _activeBuf ?? ActiveResourceDrawPercent.ToString();
             _delayBuf = _delayBuf ?? DelaySeconds.ToString();
             _messageFilterBuf = _messageFilterBuf ?? MessageFilters.Join(null, " ");
 
@@ -101,14 +124,14 @@ namespace LightsOut.Boilerplate
 
             settingListing.TextFieldNumericLabeled(
                 "LightsOut_Settings_LatentPowerDrawRateLabel".Translate(),
-                ref standbyRate, ref _standbyBuf,
+                ref StandbyResourceDrawPercent, ref _standbyBuf,
                 0, 100
                 );
 
             settingListing.TextFieldNumericLabeled(
                 "LightsOut_Settings_ActivePowerDrawRateLabel".Translate(),
-                ref activeRate, ref _activeBuf,
-                100, int.MaxValue
+                ref ActiveResourceDrawPercent, ref _activeBuf,
+                0, int.MaxValue
                 );
 
             _messageFilterBuf = settingListing.TextEntryLabeled(
@@ -117,8 +140,6 @@ namespace LightsOut.Boilerplate
                 1
                 );
 
-            StandbyResourceDrawRate = standbyRate / 100f;
-            ActiveResourceDrawRate = activeRate / 100f;
             MessageFilters = _messageFilterBuf.ToLower().Split(' ');
             UpdateLightGlowersOnSettingChange(flickLights);
             FlickLights = flickLights;
@@ -138,20 +159,28 @@ namespace LightsOut.Boilerplate
         /// </summary>
         public void ExposeSettings()
         {
-            uint standbyRate = (uint)(StandbyResourceDrawRate * 100);
-            uint activeRate = (uint)(ActiveResourceDrawRate * 100);
+            _messageFilterBuf = _messageFilterBuf ?? MessageFilters.Join(null, " ");
 
-            Scribe_Values.Look(ref FlickLights, "EverythingButLightsOut");
-            Scribe_Values.Look(ref NightLights, "NightLights");
-            Scribe_Values.Look(ref AnimalParty, "AnimalParty");
-            Scribe_Values.Look(ref DelaySeconds, "DelaySeconds");
-            Scribe_Values.Look(ref standbyRate, "LatentPowerDrawRate");
-            Scribe_Values.Look(ref activeRate, "ActivePowerDrawRate");
-            Scribe_Values.Look(ref _messageFilterBuf, "DebugFilters");
+            // clamp the active power draw to at least 100
+            if (ActiveResourceDrawPercent < 100)
+                ActiveResourceDrawPercent = 100;
 
-            StandbyResourceDrawRate = standbyRate / 100f;
-            ActiveResourceDrawRate = activeRate / 100f;
+            Scribe_Values.Look(ref FlickLights, "EverythingButLightsOut", FlickLights, true);
+            Scribe_Values.Look(ref NightLights, "NightLights", NightLights, true);
+            Scribe_Values.Look(ref AnimalParty, "AnimalParty", AnimalParty, true);
+            Scribe_Values.Look(ref DelaySeconds, "DelaySeconds", DelaySeconds, true);
+            Scribe_Values.Look(ref StandbyResourceDrawPercent, "LatentPowerDrawRate", StandbyResourceDrawPercent, true);
+            Scribe_Values.Look(ref ActiveResourceDrawPercent, "ActivePowerDrawRate", ActiveResourceDrawPercent, true);
+            Scribe_Values.Look(ref _messageFilterBuf, "DebugFilters", _messageFilterBuf, true);
+
             MessageFilters = _messageFilterBuf.ToLower().Split(' ');
+
+            // reset the buffers so that they get rebuilt the next time the settings window renders
+            // do this in case we needed to adjust an input value due to it falling out of range
+            // (otherwise the buffer would continue to show the old value that isn't acutally saved)
+            _standbyBuf = null;
+            _activeBuf = null;
+            _delayBuf = null;
         }
 
         /// <summary>
@@ -180,8 +209,8 @@ namespace LightsOut.Boilerplate
         /// <param name="newVal">The new value the setting is being set to</param>
         private void UpdateLightGlowersOnSettingChange(bool newVal)
         {
-            DebugLogger.LogInfo($"Changing FlickLights from {FlickLights} to {newVal}", DebugMessageKeys.Settings);
             if (FlickLights == newVal) return;
+            DebugLogger.LogInfo($"Changing FlickLights from {FlickLights} to {newVal}", DebugMessageKeys.Settings);
 
             FlickLights = newVal;
 
@@ -215,5 +244,98 @@ namespace LightsOut.Boilerplate
                 }
             }
         }
+
+        #region legacy HugsLib setting support
+        /// <summary>
+        /// The mod no longer uses HugsLib, but that's where settings used to be stored
+        /// 
+        /// in order to make the transition easier for users, attempt to load the old
+        /// settings from HugsLib into the current settings location as the default values
+        /// 
+        /// this should effectively restore the settings without overwriting them each time
+        /// </summary>
+        private void TryRestoreSettingsFromHugsLib()
+        {
+            // try to locate the HugsLib mod settings
+            string hugsLibPath = Path.Combine(GenFilePaths.SaveDataFolderPath, "HugsLib");
+            if (!Directory.Exists(hugsLibPath)) { return; }
+            string modSettingPath = Path.Combine(hugsLibPath, "ModSettings.xml");
+            if (!File.Exists(modSettingPath)) { return; }
+
+            // if it exists, load the old settings
+            try
+            {
+                XDocument doc = XDocument.Load(modSettingPath);
+                if (doc.Root != null)
+                {
+                    foreach (XElement modNode in doc.Root.Elements())
+                    {
+                        if (modNode.Name != "LightsOut") { continue; }
+                        Log.Message("[LightsOut] Found previous settings from HugsLib. Using them as default values.");
+                        foreach (XElement settingNode in modNode.Elements())
+                        {
+                            string value = settingNode.Value;
+                            if (settingNode.Name == "EverythingButLightsOut")
+                                TryParse(value, ref FlickLights);
+                            else if (settingNode.Name == "NightLights")
+                                TryParse(value, ref NightLights);
+                            else if (settingNode.Name == "AnimalParty")
+                                TryParse(value, ref AnimalParty);
+                            else if (settingNode.Name == "DelaySeconds")
+                                TryParse(value, ref DelaySeconds);
+                            else if (settingNode.Name == "LatentPowerDrawRate")
+                                TryParse(value, ref StandbyResourceDrawPercent);
+                            else if (settingNode.Name == "ActivePowerDrawRate")
+                                TryParse(value, ref ActiveResourceDrawPercent);
+                            else if (settingNode.Name == "DebugFilters")
+                                MessageFilters = value.ToLower().Split(' ');
+                        }
+                        // no need to loop further, we found our settings
+                        break;
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        /// <summary>
+        /// Attempts to parse the value into the specified variable
+        /// </summary>
+        /// <param name="value">The value to try to parse</param>
+        /// <param name="toMod">The variable to put the value into</param>
+        private static void TryParse(string value, ref bool toMod)
+        {
+            if (bool.TryParse(value, out bool result))
+            {
+                toMod = result;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to parse the value into the specified variable
+        /// </summary>
+        /// <param name="value">The value to try to parse</param>
+        /// <param name="toMod">The variable to put the value into</param>
+        private static void TryParse(string value, ref float toMod)
+        {
+            if (float.TryParse(value, out float result))
+            {
+                toMod = result;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to parse the value into the specified variable
+        /// </summary>
+        /// <param name="value">The value to try to parse</param>
+        /// <param name="toMod">The variable to put the value into</param>
+        private static void TryParse(string value, ref int toMod)
+        {
+            if (int.TryParse(value, out int result))
+            {
+                toMod = result;
+            }
+        }
+        #endregion
     }
 }
